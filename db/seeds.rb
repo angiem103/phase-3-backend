66fa3cb0e69554c958963225b891fb8f0ac0154a
angie <amaticorena103@hotmail.com>
2022-05-12 20:59:52 -0400

    created fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+20.times do
+    Customer.create(
+        name: Faker::Name.name,
+        phone_number: Faker::PhoneNumber.cell_phone,
+        email: Faker::Internet.email,
+        address: Faker::Address.full_address
+    )
+end
+
+20.times do
+    Order.create(
+        fulfilled: Faker::Boolean.boolean,
+        paid: Faker::Boolean.boolean,
+        customer_id: rand(1..20),
+    )
+end
+
+10.times do
+    Product.create(
+        name: Faker::Dessert.variety,
+        flavor: Faker::Dessert.flavor,
+        cost: Faker::Commerce.price(range: 0..20.0)
+    )
+end
 
 puts "✅ Done seeding!"
